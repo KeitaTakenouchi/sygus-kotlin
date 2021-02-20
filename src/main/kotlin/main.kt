14704adafd46ffaddc632415d80b91f89e5f4103
Takenouchi Keita <t.keita.osaka@gmail.com>
2021-02-21 02:47:38 +0900

    Add an example for parsing .sl file.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,16 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+import org.antlr.v4.runtime.tree.ParseTreeWalker
+
+fun main() {
+    val fileName = "benchmarks/sygus-comp-2017/array_search_3.sl"
+
+    val charStream = CharStreams.fromFileName(fileName)
+    val lexer = SygusLexer(charStream)
+    val tokenStream = CommonTokenStream(lexer)
+    val parser = SygusParser(tokenStream)
+
+    val tree = parser.start()
+    val walker = ParseTreeWalker.DEFAULT
+    walker.walk(PrintVisitor(), tree)
 }
---
+++ src/main/kotlin/parser/Visitors.kt
@@ -0,0 +1,22 @@
+import org.antlr.v4.runtime.tree.TerminalNode
+
+class PrintVisitor : SygusBaseListener() {
+
+    override fun enterNTDef(ctx: SygusParser.NTDefContext?) {
+        super.enterNTDef(ctx)
+        println()
+        print("\t")
+    }
+
+    override fun exitSetLogicCmd(ctx: SygusParser.SetLogicCmdContext?) {
+        println()
+    }
+
+    override fun exitCmd(ctx: SygusParser.CmdContext?) {
+        println()
+    }
+
+    override fun visitTerminal(node: TerminalNode?) {
+        print(node!!.text + " ")
+    }
+}
