23d4e80145ab8ed180eb0b63352e44e21211e00b
Takenouchi Keita <t.keita.osaka@gmail.com>
2021-04-30 13:34:13 +0900

    Implemented DSL functions.

---
+++ src/main/kotlin/sygus/lang/DSL.kt
@@ -0,0 +1,118 @@
+package sygus.lang
+
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+import sygus.antlr.SygusLexer
+import sygus.antlr.SygusParser
+import sygus.antlr.SygusParser.*
+
+typealias RuleName = String
+typealias TypeName = String
+
+class DSL {
+    val types = mutableMapOf<RuleName, TypeName>()
+    val produRules = mutableMapOf<RuleName, List<Term>>()
+
+    constructor(synthFunCmdStr: SMTLIB2Str) {
+        val charStream = CharStreams.fromString(synthFunCmdStr)
+        val lexer = SygusLexer(charStream)
+
+        val tokenStream = CommonTokenStream(lexer)
+        val parser = SygusParser(tokenStream)
+        val synthFunCmd = parser.synthFunCmd()
+
+        collectNTDefs(synthFunCmd).forEach { ntDef ->
+            val name = ntDef.symbol().text
+            val type = ntDef.sortExpr().text
+            types.put(name, type)
+
+            val rules: List<Term> = collectGTerms(ntDef).map { gTerm ->
+                // TODO: assign rules names in g4 grammar file instead of ad-hoc branches
+                when {
+                    gTerm.gTermStar() != null -> {
+                        val terms = collectGTerms(gTerm.gTermStar()).map {
+                            it.text
+                        }.toTypedArray()
+                        Term(gTerm.symbol().text, *terms)
+                    }
+                    gTerm.symbol() != null -> Term(gTerm.symbol().text)
+                    gTerm.literal() != null -> Term(gTerm.literal().text)
+                    else -> throw IllegalStateException("not supproted: " + gTerm.text)
+                }
+            }
+            produRules.put(name, rules)
+        }
+
+    }
+
+    private fun collectNTDefs(synthFunCmd: SynthFunCmdContext): List<NTDefContext> {
+        val ret = mutableListOf<NTDefContext>()
+
+        // obtain the first rule (always not null)
+        val nt = synthFunCmd.nTDefPlus()
+        ret.add(nt.nTDef())
+
+        // traverse tail rules
+        var tail = nt.nTDefPlusTail()
+        while (tail.nTDef() != null) {
+            ret.add(tail.nTDef())
+            tail = tail.nTDefPlusTail()
+        }
+
+        return ret
+    }
+
+    private fun collectGTerms(nfDef: NTDefContext): List<GTermContext> {
+        val ret = mutableListOf<GTermContext>()
+
+        // obtain the first term
+        val term = nfDef.gTermPlus()
+        ret.add(term.gTerm())
+
+        // traverse tail rules
+        var tail = term.gTermPlusTail()
+        while (tail.gTerm() != null) {
+            ret.add(tail.gTerm())
+            tail = tail.gTermPlusTail()
+        }
+
+        return ret
+    }
+
+    private fun collectGTerms(gTermStar: GTermStarContext): List<GTermContext> {
+        val ret = mutableListOf<GTermContext>()
+        var tail = gTermStar
+        while (tail.gTerm() != null) {
+            ret.add(tail.gTerm())
+            tail = tail.gTermStar()
+        }
+        return ret
+    }
+}
+
+class Term(val literal: String, vararg val params: String) {
+    override fun toString(): String {
+        return if (params.isEmpty())
+            literal
+        else
+            "($literal ${params.joinToString(separator = " ")})"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Term
+
+        if (literal != other.literal) return false
+        if (!params.contentEquals(other.params)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = literal.hashCode()
+        result = 31 * result + params.contentHashCode()
+        return result
+    }
+}
---
+++ src/test/kotlin/sygus/lang/DSLTest.kt
@@ -0,0 +1,54 @@
+package sygus.lang
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class DSLTest {
+
+    @Test
+    fun test01() {
+        val input = """
+           (synth-fun findIdx ( (y1 Int) (y2 Int) (y3 Int) (y4 Int) (k1 Int)) Int 
+               (
+                   (
+                       Start Int (
+                           0 1 y1 y2 (ite BoolExpr Start Start)
+                       )
+                   )
+                   (
+                       BoolExpr Bool (
+                          (< Start Start)
+                          (<= Start Start)
+                          (> Start Start)
+                          (>= Start Start)
+                       )
+                   )
+               )
+           )
+        """
+        val dsl = DSL(input)
+
+        // types
+        assertEquals("Int", dsl.types["Start"])
+        assertEquals("Bool", dsl.types["BoolExpr"])
+
+        // rules
+        assertEquals(
+            listOf(
+                Term("0"),
+                Term("1"),
+                Term("y1"),
+                Term("y2"),
+                Term("ite", "BoolExpr", "Start", "Start"),
+            ), dsl.produRules["Start"]
+        )
+        assertEquals(
+            listOf(
+                Term("<", "Start", "Start"),
+                Term("<=", "Start", "Start"),
+                Term(">", "Start", "Start"),
+                Term(">=", "Start", "Start"),
+            ), dsl.produRules["BoolExpr"]
+        )
+    }
+}
